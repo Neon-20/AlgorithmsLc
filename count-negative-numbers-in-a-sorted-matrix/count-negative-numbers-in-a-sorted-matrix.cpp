b5ee719e511977352f788386d58d43a5856b4c52
Pranav Rajveer <55043383+Neon-20@users.noreply.github.com>
2021-12-27 10:39:43 -0800

    Time: 16 ms (61.12%), Space: 10.4 MB (83.57%) - LeetHub

---
+++ count-negative-numbers-in-a-sorted-matrix/count-negative-numbers-in-a-sorted-matrix.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int countNegatives(vector<vector<int>>& grid) {
+        int cnt=0;
+        for(int i=0;i<grid.size();i++)
+        {
+           
+            for(int j=0;j<grid[i].size();j++)
+            {
+                if(grid[i][j] < 0)
+                    cnt++;
+            }
+        }
+        return cnt;
+    }
+};
