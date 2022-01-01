014e18d87831a67a72504c4d751bba81fafce3ed
Pranav Rajveer <55043383+Neon-20@users.noreply.github.com>
2022-01-01 02:27:06 -0800

    Time: 112 ms (44.44%), Space: 27.2 MB (42.46%) - LeetHub

--- check-if-a-parentheses-string-can-be-valid/check-if-a-parentheses-string-can-be-valid.cpp
+++ check-if-a-parentheses-string-can-be-valid/check-if-a-parentheses-string-can-be-valid.cpp
@@ -3,32 +3,32 @@ public:
     
  
     bool canBeValid(string s, string locked) {
-       int left=0;
-       int right=0;
-        int n=s.size();
-        if(n%2) return false;
-        for(int i=0;i<n;i++)// Traversing from left to right
+    int left=0;
+    int right=0;
+        if(s.size()%2) return false;
+           for(int i=0;i<s.size();i++)
+           {
+               if(locked[i] == '1')
+               {
+                   if(s[i] == '(')
+                       right++;
+                   else right--;
+                   if(right<0 and left<-right)
+                       return false;
+               }
+               else left++;
+           }
+        left=0;
+        right=0;
+        for(int i=s.size()-1;i>=0;i--)
         {
             if(locked[i] == '1')
             {
                 if(s[i] == '(')
                     right++;
                 else right--;
-                if(right<0 and left<-right) return false;
-            }
-            else left++;
-        }
-        
-        
-        left=0;right=0;
-        for(int i=n-1;i>=0;i--)// Traversing from right to left
-        {
-            if(locked[i] == '1')
-            {
-                if(s[i] == '(')
-                    right++;
-                else right--;
-                if(right>0 and left<right) return false;
+                if(right>0 and left<right)
+                    return false;
             }
             else left++;
         }
