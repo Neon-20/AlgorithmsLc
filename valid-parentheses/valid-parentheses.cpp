9d4a099c9f73487d0c5c0cf40081334b35d87c9c
Pranav Rajveer <55043383+Neon-20@users.noreply.github.com>
2022-01-02 11:07:12 -0800

    Time: 3 ms (11.96%), Space: 6.2 MB (96.03%) - LeetHub

---
+++ valid-parentheses/valid-parentheses.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<int> st;
+        for(char c:s)
+        {
+            if(c == '(' or c == '[' or c == '{')
+                st.push(c);// Push it in stack:
+            else
+            {
+                if(st.empty()) return false;
+                if(c == ')' and st.top()!='(')return false;
+                if(c == ']' and st.top()!='[')return false;
+                if(c == '}' and st.top()!='{')return false;
+                 st.pop();
+            }
+           
+        }
+        return st.empty();
+        
+    }
+};
