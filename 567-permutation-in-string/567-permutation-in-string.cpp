b303e26fbf1b714060a02eede87f9fce58c0214b
Pranav Rajveer <55043383+Neon-20@users.noreply.github.com>
2022-01-06 01:29:57 -0800

    Time: 8 ms (76.20%), Space: 7.3 MB (72.55%) - LeetHub

--- 567-permutation-in-string/567-permutation-in-string.cpp
+++ 567-permutation-in-string/567-permutation-in-string.cpp
@@ -1,27 +1,14 @@
 class Solution {
 public:
     bool checkInclusion(string s1, string s2) {
-        vector<int> cnt1(26,0);
-        vector<int> cnt2(26,0);
-        if(s1.size()>s2.size()) return false;
-        int i;
-        for(i=0;i<s1.size();i++)
-        {
-             cnt1[s1[i]-'a']++;//Count krlo pehle
-             cnt2[s2[i]-'a']++;
-        }
-         if(cnt1 == cnt2) return true;
-        int first=0;
-        int end=i;
-            while(end<s2.size())
-            {
-                --cnt2[s2[first]-'a'];
-                ++cnt2[s2[end]-'a'];
-                
-                if(cnt1 == cnt2) return true;
-                first++;end++;
-                    
-            }
+       vector<int> curr(26),toget(26);
+        for(char c:s1)  toget[c-'a']++;
+       for(int i=0;i<s2.size();i++)
+       {
+           curr[s2[i]-'a']++;
+           if(i>=s1.size()) curr[s2[i-s1.size()]-'a']--;
+           if(toget == curr) return true;
+       }
         return false;
         
     }
