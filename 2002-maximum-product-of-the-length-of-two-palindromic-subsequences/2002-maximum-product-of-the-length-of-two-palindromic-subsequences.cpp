04605ad6916250a79824b300429976efb50f9d8d
Pranav Rajveer <55043383+Neon-20@users.noreply.github.com>
2022-01-06 13:04:44 -0800

    Time: 654 ms (35.20%), Space: 6 MB (71.56%) - LeetHub

--- 2002-maximum-product-of-the-length-of-two-palindromic-subsequences/2002-maximum-product-of-the-length-of-two-palindromic-subsequences.cpp
+++ 2002-maximum-product-of-the-length-of-two-palindromic-subsequences/2002-maximum-product-of-the-length-of-two-palindromic-subsequences.cpp
@@ -1,50 +1,35 @@
 class Solution {
-public:
-    
-    // int max(int a,int b)
-    // {
-    //     if(a>b) return a;
-    //     else return b;
-    // }
-         int ans=0; 
+    public:
     bool isP(string &s)
     {
         int i=0;
         int j=s.size()-1;
         while(i<j)
         {
-            if(s[i]!=s[j])
-            {
-                return false;
-            }
+            if(s[i]!=s[j]) return false;
             i++;j--;
         }
-        return 1;
+        return true;
     }
-
+    int ans=0;
     void helper(string &s,int index,string &a,string &b)
     {
-        if(index>=s.length())
+        if(s.size() == index)
         {
             if(isP(a) and isP(b))
-            {
                 ans=max(ans,(int)(a.size()*b.size()));
-            }
-            return;//
+            return;
         }
-        helper(s,index+1,a,b);
-        a+=s[index];
-        helper(s,index+1,a,b);
-        a.pop_back();
-        b+=s[index];
-        helper(s,index+1,a,b);
-        b.pop_back();  
-      
-    }
-    int maxProduct(string s) {
-        // Finding the longest disjoint pallindromic subsequence
-        // We will just do it in backtracking method or we could say the
-        //brute force approach
+    a.push_back(s[index]);
+    helper(s,index+1,a,b);
+    a.pop_back();
+    b.push_back(s[index]);
+    helper(s,index+1,a,b);
+    b.pop_back();
+    helper(s,index+1,a,b);
+}
+    int maxProduct(string s)
+    {
         string a="";
         string b="";
         helper(s,0,a,b);
