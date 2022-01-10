d609e35dab2cdceb1d20c3452deb1abe06643514
Pranav Rajveer <55043383+Neon-20@users.noreply.github.com>
2022-01-10 06:25:55 -0800

    Time: 437 ms (100.00%), Space: 99 MB (85.71%) - LeetHub

--- 2135-count-words-obtained-after-adding-a-letter/2135-count-words-obtained-after-adding-a-letter.cpp
+++ 2135-count-words-obtained-after-adding-a-letter/2135-count-words-obtained-after-adding-a-letter.cpp
@@ -1,25 +1,38 @@
 class Solution {
 public:
-    int wordCount(vector<string>& s, vector<string>& t) {
-       unordered_set<string> a;
-         for(auto &x:s)
-         {
-             sort(begin(x),end(x));
-             a.insert(x);
-         }
-        int res=0;
-        for(auto &x:t)
+    int get_mask(string &s)
+    {
+        int ans=0;int mask;
+        for(auto &c :s)
         {
-            sort(begin(x),end(x));
-            for(int i=0;i<x.size();i++)
+            mask=1<<(c-'a');
+            ans+=mask;
+        }
+        return ans;
+    }
+    int wordCount(vector<string>& startWords, vector<string>& targetWords) {
+        set<int> s;
+     for(auto &c:startWords)
+     {
+         s.insert(get_mask(c));
+     }
+        int res=0;
+        int x;
+        for(auto &c:targetWords)
+        {
+            x=get_mask(c);
+            bool ok=false;
+            for(int i=0;i<26;i++)
             {
-                if(a.count(x.substr(0,i)+x.substr(i+1)))
-                   { 
-                    ++res;break;
-                   }
+                if(x&(1<<i))
+                {
+                    if(s.find(x-(1<<i))!=s.end())
+                        ok=true;
+                }
             }
+            if(ok) res++;
+            
         }
         return res;
-        
     }
 };
