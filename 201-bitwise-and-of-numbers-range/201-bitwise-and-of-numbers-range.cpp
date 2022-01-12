8824ddaf33595676c66e6bbc03ff69d1ce92d62e
Pranav Rajveer <55043383+Neon-20@users.noreply.github.com>
2022-01-13 01:37:43 +0530

    Time: 4 ms (92.33%), Space: 6 MB (32.16%) - LeetHub

---
+++ 201-bitwise-and-of-numbers-range/201-bitwise-and-of-numbers-range.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int rangeBitwiseAnd(int m, int n) {
+        //Find out the and of all numbers in the range
+        // Logic behind this is
+        // If we shift any number towards right and try to make it 1
+        // We can do the same with l and r range
+        int res=0;
+        while(m!=n)
+        {
+            m>>=1;
+            n>>=1;
+            res++;
+        }
+        return m<<res;
+    }
+};
