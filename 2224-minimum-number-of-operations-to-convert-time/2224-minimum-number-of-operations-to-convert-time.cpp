bbd92b504daf452e2aa879ca5af8e89aabce65c0
Pranav Rajveer <55043383+Neon-20@users.noreply.github.com>
2022-04-29 23:33:40 +0530

    Time: 9 ms (5.63%), Space: 6 MB (58.76%) - LeetHub

--- 2224-minimum-number-of-operations-to-convert-time/2224-minimum-number-of-operations-to-convert-time.cpp
+++ 2224-minimum-number-of-operations-to-convert-time/2224-minimum-number-of-operations-to-convert-time.cpp
@@ -1,9 +1,9 @@
 class Solution {
 public:
+    int minutes(string &st){
+ return st[0]*600+st[1]*60+st[3]*10+st[4];
+    }
     int convertTime(string current, string correct) {
-       auto minutes=[&](string &st){
-           return st[0]*600+st[1]*60+st[3]*10+st[4];
-       } ;
         int difference=minutes(correct)-minutes(current);
         return difference/60+difference%60/15+difference%15/5+difference%5;
     }
