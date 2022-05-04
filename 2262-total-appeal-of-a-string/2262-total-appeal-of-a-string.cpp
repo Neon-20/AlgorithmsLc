11d61095e3e6408dec5095bc9d95d1bd123dc265
Pranav Rajveer <55043383+Neon-20@users.noreply.github.com>
2022-05-04 19:22:34 +0530

    Time: 45 ms (69.86%), Space: 10.6 MB (55.39%) - LeetHub

--- 2262-total-appeal-of-a-string/2262-total-appeal-of-a-string.cpp
+++ 2262-total-appeal-of-a-string/2262-total-appeal-of-a-string.cpp
@@ -3,15 +3,16 @@ public:
     long long appealSum(string s) {
        //Just traverse in each string and calculate 
         //the substring which has unique letter
-        long long ans=0;
-        int cnt[26]={};
+        //Using dp
+        long long res=0;
+        long long current=0;
+        int previous[26]={};
         for(int i=0;i<s.length();i++){
-            cnt[s[i]-'a']=i+1;
-            for(int j=0;j<26;j++){
-                ans+=cnt[j];
-            }
+        current+=i+1-previous[s[i]-'a'];   
+        previous[s[i]-'a']=1+i;    
+        res+=current;
         }
-        return ans;
+        return res;
         
     }
 };
