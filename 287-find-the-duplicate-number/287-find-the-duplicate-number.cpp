ae40db3db455f0088418f0f0b3dfad9936f121bf
Pranav Rajveer <55043383+Neon-20@users.noreply.github.com>
2022-05-05 01:49:30 +0530

    Time: 186 ms (35.33%), Space: 61.3 MB (26.61%) - LeetHub

--- 287-find-the-duplicate-number/287-find-the-duplicate-number.cpp
+++ 287-find-the-duplicate-number/287-find-the-duplicate-number.cpp
@@ -2,14 +2,12 @@ class Solution {
 public:
     // const int nax=1e5+5;
     int findDuplicate(vector<int>& nums) {
-        int n=nums.size();
-        // Creating bitset of size 
-        bitset<100005> b(0);
+       bitset<100005> b(0);
         for(auto x:nums){
             if(b[x] == 0){
                 b[x]=1;
             }
-            else {
+            else{
                 return x;
             }
         }
