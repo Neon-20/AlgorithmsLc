c6dedc791922f6cb8baaf06f6ba31e0a3df9d10d
Pranav Rajveer <55043383+Neon-20@users.noreply.github.com>
2022-05-05 18:49:45 +0530

    Time: 33 ms (32.38%), Space: 19.8 MB (7.86%) - LeetHub

--- 845-longest-mountain-in-array/845-longest-mountain-in-array.cpp
+++ 845-longest-mountain-in-array/845-longest-mountain-in-array.cpp
@@ -2,15 +2,13 @@ class Solution {
 public:
     int longestMountain(vector<int>& a) {
         int n=a.size();
-        //longest mountain in array
-        // length of longest subarray
-      vector<int> prefix(n);
-      vector<int> suffix(n);
-        for(int i=0;i<n;i++){
-           if(i>0 and a[i]>a[i-1]) prefix[i]=prefix[i-1]+1;
-        }
+       vector<int> prefix(n);
+       vector<int> suffix(n);
+      for(int i=0;i<n;i++){
+          if(i>0 and a[i]>a[i-1]) prefix[i]=prefix[i-1]+1;
+      }
         for(int i=n-2;i>=0;i--){
-            if(a[i]>a[i+1])suffix[i]=suffix[i+1]+1;
+            if(a[i+1]<a[i]) suffix[i]=suffix[i+1]+1;
         }
         int ans=0;
         for(int i=0;i<n;i++){
