7bca1228f05697d5c3b4f9afef56ce816d43f335
Pranav Rajveer <55043383+Neon-20@users.noreply.github.com>
2022-05-06 22:43:34 +0530

    Time: 26 ms (91.82%), Space: 10.6 MB (54.54%) - LeetHub

--- 2262-total-appeal-of-a-string/2262-total-appeal-of-a-string.cpp
+++ 2262-total-appeal-of-a-string/2262-total-appeal-of-a-string.cpp
@@ -2,15 +2,14 @@ class Solution {
 public:
     long long appealSum(string s) {
       //total contributions
-        int prev[26]={};
         int n=s.length();
+       vector<int> cnt(26,0);
         long long ans=0;
-        for(int i=0;i<s.length();i++){
-            ans+=(i+1-prev[s[i]-'a'])*(n-i);
-            prev[s[i]-'a']=1+i;
+        for(int i=0;i<n;i++){
+            ans+=(i+1-cnt[s[i]-'a'])*(n-i);
+            cnt[s[i]-'a']=1+i;
         }
         return ans;
-        
         
     }
 };
