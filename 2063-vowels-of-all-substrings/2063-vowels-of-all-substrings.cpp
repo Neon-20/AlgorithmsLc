27338e7b9bfc64ed2fbce75766bcfacbc84d0e3b
Pranav Rajveer <55043383+Neon-20@users.noreply.github.com>
2022-05-06 22:54:15 +0530

    Time: 37 ms (43.52%), Space: 11.1 MB (23.49%) - LeetHub

--- 2063-vowels-of-all-substrings/2063-vowels-of-all-substrings.cpp
+++ 2063-vowels-of-all-substrings/2063-vowels-of-all-substrings.cpp
@@ -3,7 +3,7 @@ public:
     long long countVowels(string s) {
         //in every substring of the word
         bool v[26]={1,0,0,0,1,0,0,0,1,0,0,0,0,0,1,0,0,0,0,0,1,0,0,0,0,0};
-        long long n=s.size(); 
+        long long n=s.size();
         long long ans=0;
         for(int i=0;i<n;i++){
             ans+=v[s[i]-'a']*(i+1)*(n-i);
