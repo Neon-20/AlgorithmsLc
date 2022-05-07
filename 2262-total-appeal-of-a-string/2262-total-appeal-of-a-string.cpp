d4b30d819499baf89e51c1ee8234bb8ff1c864a1
Pranav Rajveer <55043383+Neon-20@users.noreply.github.com>
2022-05-07 22:13:12 +0530

    Time: 19 ms (98.45%), Space: 10.6 MB (79.32%) - LeetHub

--- 2262-total-appeal-of-a-string/2262-total-appeal-of-a-string.cpp
+++ 2262-total-appeal-of-a-string/2262-total-appeal-of-a-string.cpp
@@ -2,14 +2,16 @@ class Solution {
 public:
     long long appealSum(string s) {
       //total contributions
-        int n=s.length();
-       vector<int> cnt(26,0);
+        //number of distinct strings in the substring
+        int cnt[26]={0};
         long long ans=0;
-        for(int i=0;i<n;i++){
+        int n=s.length();
+        for(int i=0;i<s.size();i++){
             ans+=(i+1-cnt[s[i]-'a'])*(n-i);
-            cnt[s[i]-'a']=1+i;
+            cnt[s[i]-'a']=i+1;
         }
         return ans;
+        
         
     }
 };
