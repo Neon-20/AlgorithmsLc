98a1d932d85f6a98400339693b3ff10691579cf4
Pranav Rajveer <55043383+Neon-20@users.noreply.github.com>
2022-05-10 15:58:49 +0530

    Time: 11 ms (87.76%), Space: 11.2 MB (17.18%) - LeetHub

--- 1-two-sum/1-two-sum.cpp
+++ 1-two-sum/1-two-sum.cpp
@@ -6,7 +6,7 @@ public:
        vector<int> ans;
         int n=nums.size();
         for(int i=0;i<n;i++){
-            if(mp.find(target-nums[i])!=mp.end()){//already present
+            if(mp.count(target-nums[i])){//already present
                 ans.push_back(mp[target-nums[i]]);
                 ans.push_back(i);
                 break;
