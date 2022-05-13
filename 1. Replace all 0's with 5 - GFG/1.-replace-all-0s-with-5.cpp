b8ff44df48cbbbe0414326ee92b3f25fdc4c650f
Pranav Rajveer <55043383+Neon-20@users.noreply.github.com>
2022-05-13 11:36:57 +0530

    Added solution - LeetHub

---
+++ 1. Replace all 0's with 5 - GFG/1.-replace-all-0s-with-5.cpp
@@ -0,0 +1,34 @@
+// { Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+int convertFive(int n);
+
+// Driver program to test above function
+int main() {
+    int T;
+    cin >> T;
+    while (T--) {
+        int n;
+        cin >> n;
+        cout << convertFive(n) << endl;
+    }
+}// } Driver Code Ends
+
+
+/*you are required to complete this method*/
+int convertFive(int n) {
+    int number=0;
+    int res=1;
+    while(n>0){
+        int var=n%10;
+        if(var == 0){
+            var=5;
+        }
+        //Create a number 
+        number=number+var*res;//
+        res=res*10;
+        n/=10;
+    }
+    return number;
+}
