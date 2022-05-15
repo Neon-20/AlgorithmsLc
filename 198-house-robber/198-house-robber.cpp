6a54c7241933da1ccea456834c15f110f1377d4b
Pranav Rajveer <55043383+Neon-20@users.noreply.github.com>
2022-05-15 13:43:37 +0530

    Time: 0 ms (100.00%), Space: 7.7 MB (76.74%) - LeetHub

--- 198-house-robber/198-house-robber.cpp
+++ 198-house-robber/198-house-robber.cpp
@@ -1,15 +1,14 @@
 class Solution {
 public:
-    int recur(vector<int> &dp,int index,vector<int> &nums){
-        if(index>=nums.size()) return 0;
-        if(dp[index]!=-1) return dp[index];
-        else
-    dp[index]=max(recur(dp,index+1,nums),nums[index]+recur(dp,index+2,nums));
-return dp[index];
-}
     int rob(vector<int>& nums) {
-        int n=nums.size();
-        vector<int> dp(n+1,-1);
-        return recur(dp,0,nums);
+       int n=nums.size();
+        int pre=0;
+        int current=0;
+        for(int i=0;i<n;i++){
+           int temp=max(pre+nums[i],current);
+           pre=current;
+           current=temp;
+        }
+        return current;
     }
 };
