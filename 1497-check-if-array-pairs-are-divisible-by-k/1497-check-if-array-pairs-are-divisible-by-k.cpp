4f8e537a5b9452d09d2e29cc1eb6686527db6981
Pranav Rajveer <55043383+Neon-20@users.noreply.github.com>
2022-05-18 18:00:15 +0530

    Time: 159 ms (81.36%), Space: 61.6 MB (89.10%) - LeetHub

---
+++ 1497-check-if-array-pairs-are-divisible-by-k/1497-check-if-array-pairs-are-divisible-by-k.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool canArrange(vector<int>& nums, int k) {
+        int n=nums.size();
+        vector<int> cnt(k);
+       for(int x:nums){
+           x%=k;
+           if(x<0) x+=k;//formal remainder
+           cnt[x]++;
+       }
+        if(cnt[0]%2!=0) return false;
+        for(int i=1;i<=k/2;i++){
+            if(cnt[i]!=cnt[k-i]) return false;
+        }
+        return true;
+    }
+};
