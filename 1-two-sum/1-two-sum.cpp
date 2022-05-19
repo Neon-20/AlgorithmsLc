5526a850fe72dff53c54b9fa7e3de76e86734d6e
Pranav Rajveer <55043383+Neon-20@users.noreply.github.com>
2022-05-19 22:10:41 +0530

    Time: 20 ms (55.46%), Space: 12.4 MB (6.26%) - LeetHub

--- 1-two-sum/1-two-sum.cpp
+++ 1-two-sum/1-two-sum.cpp
@@ -2,18 +2,16 @@ class Solution {
 public:
     // #define pb push_back
     vector<int> twoSum(vector<int>& nums, int target) {
-       map<int,int> mp;
-       vector<int> ans;
+        // int ans=0;
+        map<int,int> mp;
+        vector<int> ans;
         int n=nums.size();
         for(int i=0;i<n;i++){
-            if(mp.count(target-nums[i])){//already present
+            if(mp.count(target-nums[i])){
                 ans.push_back(mp[target-nums[i]]);
                 ans.push_back(i);
-                break;
             }
-            else{
-                mp[nums[i]]=i;
-            }
+            mp[nums[i]]=i;
         }
         return ans;
     }
