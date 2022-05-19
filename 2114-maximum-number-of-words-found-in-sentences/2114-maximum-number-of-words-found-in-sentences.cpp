aa9f4bb0523b6e63099303891b273af70df7fd15
Pranav Rajveer <55043383+Neon-20@users.noreply.github.com>
2022-05-19 12:07:34 +0530

    Time: 14 ms (65.94%), Space: 10.4 MB (17.41%) - LeetHub

--- 2114-maximum-number-of-words-found-in-sentences/2114-maximum-number-of-words-found-in-sentences.cpp
+++ 2114-maximum-number-of-words-found-in-sentences/2114-maximum-number-of-words-found-in-sentences.cpp
@@ -2,15 +2,9 @@ class Solution {
 public:
     int mostWordsFound(vector<string>& sentences) {
         int ans=0;
-       for(int i=0;i<sentences.size();i++){
-           int cnt=1;
-           for(int j=0;j<sentences[i].size();j++){
-               if(sentences[i][j]==' '){
-                   cnt++;
-               }
-           }
-           ans=max(ans,cnt);
-       }
+        for(auto sentence:sentences){
+            ans=max(ans,(int)count(begin(sentence),end(sentence),' ')+1);
+        }
         return ans;
     }
 };
