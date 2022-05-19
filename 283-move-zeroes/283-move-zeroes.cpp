69e823485614ab1d994ed28282667e6233719536
Pranav Rajveer <55043383+Neon-20@users.noreply.github.com>
2022-05-19 12:19:06 +0530

    Time: 28 ms (66.93%), Space: 19.3 MB (58.51%) - LeetHub

--- 283-move-zeroes/283-move-zeroes.cpp
+++ 283-move-zeroes/283-move-zeroes.cpp
@@ -1,18 +1,20 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        int n=nums.size();
-        int j=0;
-        for(int i=0;i<n;i++){
+          int j=0;//as a flag pointer.. basically 2 pointer approach
+        for(int i=0;i<nums.size();i++){
             if(nums[i]!=0){
-                nums[j++]=nums[i];
-            }
-        }//done
-        //noW
-        while(j<nums.size()){
-            nums[j++]=0;
+                nums[j]=nums[i];
+                j++;
+            }//now we filled the array without zeros
+            //now fill with zeros in last
         }
-    
+        while(j<nums.size())
+        {
+            nums[j]=0;
+            j++;
+        }
         
+            
     }
 };
