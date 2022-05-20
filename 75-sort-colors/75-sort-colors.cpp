6cde60d3e1a181d89c0fb43d5ffc05bf624a82dd
Pranav Rajveer <55043383+Neon-20@users.noreply.github.com>
2022-05-20 15:55:15 +0530

    Time: 4 ms (41.75%), Space: 8.3 MB (71.29%) - LeetHub

--- 75-sort-colors/75-sort-colors.cpp
+++ 75-sort-colors/75-sort-colors.cpp
@@ -2,13 +2,20 @@ class Solution {
 public:
     void sortColors(vector<int>& nums) {
         int n=nums.size();
-    //Using 2 pointer technique we have as
-        int left=0;
-        int right=n-1;
-        for(int i=0;i<n;i++){
-            while(nums[i] == 2 and i<right) swap(nums[i],nums[right--]);
-            while(nums[i] == 0 and i>left) swap(nums[i],nums[left++]);
+    int low=0;
+        int high=n-1;
+        int res=0;
+        while(res<=high){
+            if(nums[res]==0){
+                swap(nums[low++],nums[res++]);
+            }
+            else if(nums[res]==1){
+                res++;
+            }
+            else{
+                swap(nums[high],nums[res]);
+                high--;
+            }
         }
-        
     }
 };
