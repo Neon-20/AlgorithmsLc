d850dcf809a8e13896fdd79e36b7d9a209b20733
Pranav Rajveer <55043383+Neon-20@users.noreply.github.com>
2022-05-21 22:36:56 +0530

    Time: 292 ms (24.85%), Space: 85.5 MB (66.51%) - LeetHub

--- 2270-number-of-ways-to-split-array/2270-number-of-ways-to-split-array.cpp
+++ 2270-number-of-ways-to-split-array/2270-number-of-ways-to-split-array.cpp
@@ -2,19 +2,14 @@ class Solution {
 public:
     #define ll long long
     int waysToSplitArray(vector<int>& nums) {
-        //number of ways to split array
-        //calculate the prefix sum and cnt 
         int n=size(nums);
-      vector<long long> prefix(n);
-        prefix[0]=nums[0];
-        for(int i=1;i<n;i++){
-            prefix[i]=prefix[i-1]+nums[i];
-        }
+        ll left=0;
+        ll right=0;
         ll cnt=0;
-        ll left=0,right=0;
+        for(int i=0;i<n;i++) right+=nums[i];
         for(int i=0;i<n-1;i++){
-            left=prefix[i];
-            right=prefix[n-1]-prefix[i];
+            left+=nums[i];
+            right-=nums[i];
             if(left>=right) cnt++;
         }
         return cnt;
