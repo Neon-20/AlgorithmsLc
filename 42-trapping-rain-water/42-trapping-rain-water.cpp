ac1be92ce1f9c642b39e27429977fabd3711ce21
Pranav Rajveer <55043383+Neon-20@users.noreply.github.com>
2022-05-22 16:22:27 +0530

    Time: 16 ms (64.70%), Space: 19.8 MB (5.72%) - LeetHub

--- 42-trapping-rain-water/42-trapping-rain-water.cpp
+++ 42-trapping-rain-water/42-trapping-rain-water.cpp
@@ -1,19 +1,37 @@
 class Solution {
 public:
     int trap(vector<int>& nums) {
-      int n=size(nums);
-       vector<int> prefix(n);
-        vector<int> suffix(n);
-        for(int i=1;i<n;i++){
-            prefix[i]=max(nums[i-1],prefix[i-1]);
-        }
-        for(int i=n-2;i>=0;i--){
-            suffix[i]=max(suffix[i+1],nums[i+1]);
-        }
+     //Trapping rainwater;
+        //Time-> O(n), Space-O(1) Approach->Two pointers
+        if(size(nums)<=2) return 0;
+        int left=0;
+        int n=size(nums);
+        int right=n-1;
+        int largestleft=0;
+        int largestright=0;
         int ans=0;
-        for(int i=0;i<n;i++){
-            int level=min(prefix[i],suffix[i]);
-            if(level>=nums[i]) ans+=level-nums[i];
+        while(left<right){
+            if(nums[right]>nums[left]){
+                if(nums[left]>=largestleft)
+                {
+                    largestleft=nums[left];
+                }
+                else {
+                    ans+=largestleft-nums[left];
+                }
+                left++;
+               }
+            else{
+                if(nums[right]>=largestright)
+                {
+                    largestright=nums[right];
+                }
+                 else {
+                     ans+=largestright-nums[right];
+                 }
+                    right--;
+               }
+            
         }
         return ans;
     }
