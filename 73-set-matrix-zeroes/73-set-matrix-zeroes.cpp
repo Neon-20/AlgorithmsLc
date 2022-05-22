fd2d3b706f58eeb7e5d006803008f16c2a6f8b26
Pranav Rajveer <55043383+Neon-20@users.noreply.github.com>
2022-05-22 11:57:14 +0530

    Time: 39 ms (5.41%), Space: 13.2 MB (82.32%) - LeetHub

--- 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
@@ -1,21 +1,22 @@
 class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
-      bool rz=false;
-        bool cz=false;
-        for(int i=0;i<size(matrix);i++){
-            if(matrix[i][0]==0){
+     bool rz=false;//first cell
+     bool cz=false;//first cell
+        for(int i=0;i<matrix.size();i++){
+            if(matrix[i][0] == 0){
                 cz=true;
                 break;
             }
         }
-        for(int i=0;i<size(matrix[0]);i++){
-            if(matrix[0][i]==0){
+        for(int i=0;i<matrix[0].size();i++){
+            if(matrix[0][i] == 0){
                 rz=true;
                 break;
             }
         }
-        for(int i=1;i<size(matrix);i++){
+        // checking except first cell
+        for(int i=1;i<matrix.size();i++){
             for(int j=1;j<size(matrix[0]);j++){
                 if(matrix[i][j] == 0){
                     matrix[i][0]=0;
@@ -23,20 +24,22 @@ public:
                 }
             }
         }
-         for(int i=1;i<size(matrix);i++){
+        //process the matrix
+        for(int i=1;i<matrix.size();i++){
             for(int j=1;j<size(matrix[0]);j++){
                 if(matrix[i][0] == 0 or matrix[0][j] == 0){
-                   matrix[i][j]=0;
+                    matrix[i][j]=0;
                 }
             }
         }
+        //check for all at last
         if(rz){
-            for(int i=0;i<size(matrix[0]);i++){
+            for(int i=0;i<matrix[0].size();i++){
                 matrix[0][i]=0;
             }
         }
-        if(cz){
-            for(int i=0;i<size(matrix);i++){
+         if(cz){
+            for(int i=0;i<matrix.size();i++){
                 matrix[i][0]=0;
             }
         }
