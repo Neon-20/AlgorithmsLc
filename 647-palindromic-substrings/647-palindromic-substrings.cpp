7f9470c95184de0f895b265412ea454011a0b8d9
Pranav Rajveer <55043383+Neon-20@users.noreply.github.com>
2022-05-22 17:27:14 +0530

    Time: 80 ms (15.40%), Space: 10.6 MB (41.74%) - LeetHub

--- 647-palindromic-substrings/647-palindromic-substrings.cpp
+++ 647-palindromic-substrings/647-palindromic-substrings.cpp
@@ -1,23 +1,20 @@
 class Solution {
 public: 
-    bool pallindrome(string s){
-    for(int i=0;i<size(s);i++){
-        if(s[i]!=s[size(s)-i-1]){
-            return false;
-        }
+    int dp[1005][1005];
+    int found(string &s, int i,int j){
+        if(i>=j) return 1;
+        if(dp[i][j]!=-1) return dp[i][j];
+        if(s[i]!=s[j]) return dp[i][j]=0;
+        return dp[i][j]=found(s,i+1,j-1);
     }
-    return true;
-}
     int countSubstrings(string s) {
-        int begin,end;
-        int ans=0;
-        for(int i = 0; i < s.size(); i++) {
-            begin = i, end = i;
-            while(begin >= 0 && end<s.size() && s[begin--]==s[end++]) ans++;
-            begin = i - 1, end = i;
-            while(begin >= 0 && end<s.size() && s[begin--]==s[end++]) ans++;
-        }
-        
-        return ans;
+       memset(dp,-1,sizeof(dp));
+        int cnt=0;
+       for(int i=0;i<size(s);i++){
+           for(int j=i;j<size(s);j++){
+               if(found(s,i,j)) cnt++;
+           }
+       }
+        return cnt;
     }
 };
