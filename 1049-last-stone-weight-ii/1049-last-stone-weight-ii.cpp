a3554d3fd50cf82fad35864935d531ca3b4dc4a5
Pranav Rajveer <55043383+Neon-20@users.noreply.github.com>
2022-05-22 11:20:57 +0530

    Time: 11 ms (35.07%), Space: 8.7 MB (36.87%) - LeetHub

--- 1049-last-stone-weight-ii/1049-last-stone-weight-ii.cpp
+++ 1049-last-stone-weight-ii/1049-last-stone-weight-ii.cpp
@@ -1,24 +1,23 @@
 class Solution {
 public:
-     int dp[35][5050];
-        int solve(vector<int> &stones,int idx,int cal,int sum,int n){
-            if(idx>=n){
-                return abs(sum-cal);
-            }
-            if(dp[idx][sum]!=-1) return dp[idx][sum];
-int ans=min(solve(stones,idx+1,cal,sum,n),
-            solve(stones,idx+1,cal+stones[idx],sum-stones[idx],n));
+    int dp[35][5050];//denoting i and sum dp[i][sum]
+    int solve(vector<int> &stones,int idx,int add,int sum,int n){
+        if(idx>=n) return abs(sum-add);
+        if(dp[idx][sum]!=-1) return dp[idx][sum];
+        int ans=0;
+        ans=min(solve(stones,idx+1,add,sum,n),
+               solve(stones,idx+1,add+stones[idx],sum-stones[idx],n));
         dp[idx][sum]=ans;
-            return ans;
+        return ans;
     }
     int lastStoneWeightII(vector<int>& stones) {
-        //Knapsack DP problem
+       //We have 2 choices
+        //1. Either to take the stone equal in worth
+        //2. Or to take the stone not equal ok 
         int n=size(stones);
-       memset(dp,-1,sizeof(dp));
-            int sum=0;
-       for(int x:stones){
-           sum+=x;
-       }
-            return solve(stones,0,0,sum,n);
+        memset(dp,-1,sizeof(dp));
+        int sum=0;
+        for(int x:stones) sum+=x;
+        return solve(stones,0,0,sum,n);
     }
 };
