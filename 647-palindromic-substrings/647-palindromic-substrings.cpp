4b64560b6798eccf63337e480706348a250a8905
Pranav Rajveer <55043383+Neon-20@users.noreply.github.com>
2022-05-22 17:28:32 +0530

    Time: 70 ms (16.15%), Space: 10.7 MB (35.24%) - LeetHub

--- 647-palindromic-substrings/647-palindromic-substrings.cpp
+++ 647-palindromic-substrings/647-palindromic-substrings.cpp
@@ -4,8 +4,11 @@ public:
     int found(string &s, int i,int j){
         if(i>=j) return 1;
         if(dp[i][j]!=-1) return dp[i][j];
-        if(s[i]!=s[j]) return dp[i][j]=0;
-        return dp[i][j]=found(s,i+1,j-1);
+        if(s[i]!=s[j]) return 0;
+        int ans=0;
+        ans=found(s,i+1,j-1);
+         dp[i][j]=ans;
+        return ans;
     }
     int countSubstrings(string s) {
        memset(dp,-1,sizeof(dp));
