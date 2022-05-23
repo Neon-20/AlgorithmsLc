25b7085c578c38bb3f3d8eec4fcdb77924eeb0cd
Pranav Rajveer <55043383+Neon-20@users.noreply.github.com>
2022-05-23 20:33:12 +0530

    Time: 577 ms (26.05%), Space: 38.1 MB (32.69%) - LeetHub

--- 474-ones-and-zeroes/474-ones-and-zeroes.cpp
+++ 474-ones-and-zeroes/474-ones-and-zeroes.cpp
@@ -1,23 +1,20 @@
 class Solution {
 public:
-    int cache[601][101][101];
-    int solve(vector<string> &str,int idx,int m,int n){
-        if(idx == size(str)) return 0;
-        if(cache[idx][m][n]!=-1) return cache[idx][m][n];
-        int zero=0,one=0;
-        for(auto x:str[idx]){
-            if(x == '0') zero++;
-            if(x == '1') one++;
-        }
-        int result=0;
-        // int ans=0;
-        if(zero<=m and one<=n){
-            result=max(result,solve(str,idx+1,m-zero,n-one)+1);
-        }
-    result=max(result,solve(str,idx+1,m,n));
-        cache[idx][m][n]=result;
-        return result;  
-    }
+    int cache[605][105][105];
+   int solve(vector<string> &str,int idx,int m,int n){
+       if(idx == size(str)) return 0;
+       if(cache[idx][m][n]!=-1) return cache[idx][m][n];
+       int zero=count(begin(str[idx]),end(str[idx]),'0');
+       int one=count(begin(str[idx]),end(str[idx]),'1');
+       // int one=size(str[idx])-zero;
+       int ans=0;
+       if(zero<=m and one<=n){
+           ans=max(ans,solve(str,idx+1,m-zero,n-one)+1);
+       }
+        ans=max(ans,solve(str,idx+1,m,n));
+       cache[idx][m][n]=ans;
+       return ans;
+   }
     int findMaxForm(vector<string>& strs, int m, int n) {
         memset(cache,-1,sizeof(cache));
         return solve(strs,0,m,n);
