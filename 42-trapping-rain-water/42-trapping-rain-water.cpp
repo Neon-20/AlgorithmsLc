4f8d7400917a1cfaba41ca49facb06131842bd2a
Pranav Rajveer <55043383+Neon-20@users.noreply.github.com>
2022-05-24 11:06:31 +0530

    Time: 12 ms (85.24%), Space: 20.7 MB (5.59%) - LeetHub

--- 42-trapping-rain-water/42-trapping-rain-water.cpp
+++ 42-trapping-rain-water/42-trapping-rain-water.cpp
@@ -3,35 +3,21 @@ public:
     int trap(vector<int>& nums) {
      //Trapping rainwater;
         //Time-> O(n), Space-O(1) Approach->Two pointers
-        if(size(nums)<=2) return 0;
-        int left=0;
-        int n=size(nums);
-        int right=n-1;
-        int largestleft=0;
-        int largestright=0;
+      int n=size(nums);
+        vector<int> prefix(n);
+        vector<int> suffix(n);
+        //first find the max left value
+        for(int i=1;i<n;i++){
+            prefix[i]=max(prefix[i-1],nums[i-1]);
+        }
+        for(int i=n-2;i>=0;i--){
+            suffix[i]=max(suffix[i+1],nums[i+1]);
+        }
+        //calculated for the both left and right parts
         int ans=0;
-        while(left<right){
-            if(nums[right]>nums[left]){
-                if(nums[left]>=largestleft)
-                {
-                    largestleft=nums[left];
-                }
-                else {
-                    ans+=largestleft-nums[left];
-                }
-                left++;
-               }
-            else{
-                if(nums[right]>=largestright)
-                {
-                    largestright=nums[right];
-                }
-                 else {
-                     ans+=largestright-nums[right];
-                 }
-                    right--;
-               }
-            
+        for(int i=0;i<n;i++){
+         int level=min(prefix[i],suffix[i]);
+        if(level>=nums[i]) ans+=level-nums[i];
         }
         return ans;
     }
