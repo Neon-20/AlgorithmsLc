bee6fdfd739939211dbe84bb9837f49495a59852
Pranav Rajveer <55043383+Neon-20@users.noreply.github.com>
2022-05-28 01:57:39 +0530

    Time: 124 ms (10.13%), Space: 45.7 MB (8.03%) - LeetHub

--- 1423-maximum-points-you-can-obtain-from-cards/1423-maximum-points-you-can-obtain-from-cards.cpp
+++ 1423-maximum-points-you-can-obtain-from-cards/1423-maximum-points-you-can-obtain-from-cards.cpp
@@ -2,17 +2,20 @@ class Solution {
 public:
     int maxScore(vector<int>& nums, int k) {
         int n=size(nums);
-       //front and back sliding window magic
-        int sum=0;
-        int ans=0;
-        for(int i=n-k;i<n;i++){
-            ans+=nums[i];
+       vector<int> prefix(n+1,0);
+       vector<int> suffix(n+1,0);
+        for(int i=0;i<n;i++){
+            prefix[i+1]=prefix[i]+nums[i];
         }
-         sum=ans;
-        for(int i=0;i<k;i++){
-            sum-=nums[n-k+i];
-          sum+=nums[i];
-            ans=max(ans,sum);
+        for(int i=n-1;i>=0;i--){
+            suffix[i]=suffix[i+1]+nums[i];
+        }
+        int ans=INT_MIN;
+        reverse(begin(suffix),end(suffix));
+        
+        for(int i=0;i<=k;i++)
+        {
+            ans=max(ans,suffix[i]+prefix[k-i]);
         }
         return ans;
     }
