34392a25e98514e7da4eb820548875f8f85735b2
Pranav Rajveer <55043383+Neon-20@users.noreply.github.com>
2022-05-28 13:11:10 +0530

    Time: 702 ms (32.57%), Space: 87.3 MB (77.36%) - LeetHub

--- 1770-maximum-score-from-performing-multiplication-operations/1770-maximum-score-from-performing-multiplication-operations.cpp
+++ 1770-maximum-score-from-performing-multiplication-operations/1770-maximum-score-from-performing-multiplication-operations.cpp
@@ -2,20 +2,22 @@ class Solution {
 public:
     int cache[1001][1001];
     //reduce it into 2d dp, picking elements from left and right
-    int solve(int left,int idx,vector<int> &nums, vector<int> &mul){
-        if(idx == size(mul)) return 0;
+    int solve(int left,int idx,vector<int> &nums,vector<int> &mul){
+        int n=size(nums);
+       if(idx == size(mul)) return 0;
         if(cache[left][idx]!=-1) return cache[left][idx];
+        int score=0;
         int pickleft=solve(left+1,idx+1,nums,mul)+nums[left]*mul[idx];
-        int right=solve(left,idx+1,nums,mul)+nums[size(nums)-(idx-left)-1]*mul[idx];
-       return cache[left][idx]=max(pickleft,right);
-       
+int right=solve(left,idx+1,nums,mul)+nums[n-(idx-left)-1]*mul[idx];
+        score=max(pickleft,right);
+        cache[left][idx]=score;
+        return score;
     }
     int maximumScore(vector<int>& nums, vector<int>& mul) {
         // choosing the integers is the key to the problem
-        int n=size(nums);
-        int m=size(mul);
+      int n=nums.size();
+      int m=nums.size();
         memset(cache,-1,sizeof(cache));
         return solve(0,0,nums,mul);
-        
     }
 };
