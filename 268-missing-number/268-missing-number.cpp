ee4ed1f4b0a145072a0458badd825185dd126e50
Pranav Rajveer <55043383+Neon-20@users.noreply.github.com>
2022-05-28 11:03:56 +0530

    Time: 72 ms (5.13%), Space: 25.6 MB (5.21%) - LeetHub

--- 268-missing-number/268-missing-number.cpp
+++ 268-missing-number/268-missing-number.cpp
@@ -1,7 +1,16 @@
 class Solution {
 public:
     int missingNumber(vector<int>& nums) {
-int n=size(nums);      
-        return n*(n+1)/2- accumulate(begin(nums),end(nums),0);
+int n=size(nums);  
+   map<int,int> mp;
+        for(int x:nums){
+            mp[x]++;
+        }
+        for(int i=0;i<=n;i++){
+            if(mp[i] == 0){
+                return i;
+            }
+        }
+        return -1;
     }
 };
