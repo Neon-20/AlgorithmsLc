33955483c979fcb1d76e6286719ce654a35d49d7
Pranav Rajveer <55043383+Neon-20@users.noreply.github.com>
2022-05-29 00:04:03 +0530

    Time: 438 ms (13.54%), Space: 144.7 MB (43.54%) - LeetHub

--- 111-minimum-depth-of-binary-tree/111-minimum-depth-of-binary-tree.cpp
+++ 111-minimum-depth-of-binary-tree/111-minimum-depth-of-binary-tree.cpp
@@ -12,12 +12,16 @@
 class Solution {
 public:
     int minDepth(TreeNode* root) {
-        //again and left part of the tree recursion
+      //Bfs works better here as solution
         if(root == nullptr) return 0;
-        int left=minDepth(root->left);
-        int right=minDepth(root->right);
-        if(root->left == nullptr) return 1+right;
-        if(root->right == nullptr) return 1+left;
-        return 1+min(left,right);
+         if(root->left == nullptr and root->right!=nullptr){
+           return 1+minDepth(root->right); 
+        }
+       else if(root->right == nullptr and root->left!=nullptr){
+           return 1+minDepth(root->left); 
+        }
+       else{
+           return 1+min(minDepth(root->left),minDepth(root->right));
+       }
     }
 };
