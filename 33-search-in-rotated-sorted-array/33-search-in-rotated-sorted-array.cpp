bff60079fc9835747a095e60dd6f98fb6779b7fe
Pranav Rajveer <55043383+Neon-20@users.noreply.github.com>
2022-06-01 14:31:52 +0530

    Time: 10 ms (19.80%), Space: 11.1 MB (30.10%) - LeetHub

--- 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.cpp
@@ -1,28 +1,30 @@
 class Solution {
 public:
-    int search(vector<int>& nums, int target) {
+    int search(vector<int>& A, int target) {
         //if it is in nums or if it is not in nums
         //Use binary search
-        int left=0;
-        int right=size(nums)-1;
-        while(left<right){
-            int mid=(left+right)/2;
-            if(nums[mid] == target) return mid;
-            if(nums[left]<=nums[mid]){//left pointer se bade ho aap
-                if(target<nums[mid] and target>=nums[left]){
-                    right=mid-1;// portion search
-                }
-                else{
-                    left=mid+1;
-                }
+        int n=size(A);
+      int lo = 0;
+    int hi = n - 1;
+    while (lo < hi) {
+        int mid = lo + (hi-lo)/ 2;
+        if (A[mid] == target) return mid;
+        
+        if (A[lo] <= A[mid]) {
+            if (target >= A[lo] && target < A[mid]) {
+                hi = mid - 1;
+            } else {
+                lo = mid + 1;
             }
-            else   {//right sorted portion
-                if(target>nums[mid] and target<=nums[right]){
-                    left=mid+1;
-                }
-                else right=mid-1;
-            }  
+        } else {
+            if (target > A[mid] && target <= A[hi]) {
+                lo = mid + 1;
+            } else {
+                hi = mid - 1;
+            }
         }
-        return nums[left]==target ? left:-1;
+    }
+    if(A[lo]==target) return lo;
+        else return -1;
     }
 };
