79bca22c571d88858218f6b6c2a1d27dd8dd72aa
Pranav Rajveer <55043383+Neon-20@users.noreply.github.com>
2022-06-05 11:51:25 +0530

    Time: 20 ms (24.10%), Space: 7.8 MB (21.92%) - LeetHub

---
+++ 52-n-queens-ii/52-n-queens-ii.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+     unordered_set<int> cols,pos,neg;
+    int cnt=0;
+    void dfs(int row,int n,vector<string> &boards){
+        if(row>=n){
+            cnt++;
+            return;
+        }
+       for(int col=0;col<n;col++){
+           if(cols.count(col) or pos.count(row+col) or 
+              neg.count(row-col))
+           {
+               continue;
+           }
+           else{
+               cols.insert(col);
+               pos.insert(row+col);
+               neg.insert(row-col);
+               boards[row][col]='Q';
+               dfs(row+1,n,boards);
+               cols.erase(col);
+               pos.erase(row+col);
+               neg.erase(row-col);
+               boards[row][col]='.';
+           }
+       }
+    }
+    int totalNQueens(int n) {
+        //for returning the number of distinct solutions we have as
+        vector<string> boards(n,string(n,'.'));
+        dfs(0,n,boards);
+        return cnt;
+    }
+};
