4106089f3d45d9c8423abd692a35cb10a10ccf12
Pranav Rajveer <55043383+Neon-20@users.noreply.github.com>
2022-06-05 16:12:37 +0530

    Time: 10 ms (42.96%), Space: 8 MB (21.69%) - LeetHub

--- 52-n-queens-ii/52-n-queens-ii.cpp
+++ 52-n-queens-ii/52-n-queens-ii.cpp
@@ -7,28 +7,27 @@ public:
             cnt++;
             return;
         }
-       for(int col=0;col<n;col++){
-           if(cols.count(col) or pos.count(row+col) or 
-              neg.count(row-col))
-           {
-               continue;
-           }
-           else{
-               cols.insert(col);
-               pos.insert(row+col);
-               neg.insert(row-col);
-               boards[row][col]='Q';
-               dfs(row+1,n,boards);
-               cols.erase(col);
-               pos.erase(row+col);
-               neg.erase(row-col);
-               boards[row][col]='.';
-           }
-       }
+        for(int col=0;col<n;col++){
+    if(cols.count(col) or pos.count(row+col) or
+       neg.count(row-col)){
+        continue;
     }
+        else{
+            cols.insert(col);
+            pos.insert(row+col);
+            neg.insert(row-col);
+            boards[row][col]='Q';
+            dfs(row+1,n,boards);
+            cols.erase(col);
+            pos.erase(row+col);
+            neg.erase(row-col);
+            boards[row][col]='.';
+        }
+    }
+}
     int totalNQueens(int n) {
         //for returning the number of distinct solutions we have as
-        vector<string> boards(n,string(n,'.'));
+       vector<string> boards(n,string(n,'.'));
         dfs(0,n,boards);
         return cnt;
     }
