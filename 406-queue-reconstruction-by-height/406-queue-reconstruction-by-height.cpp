cba0495c3232b3f31239ae6a713d1b557c83d80c
Pranav Rajveer <55043383+Neon-20@users.noreply.github.com>
2022-06-08 10:11:40 +0530

    Time: 285 ms (16.25%), Space: 25 MB (7.12%) - LeetHub

--- 406-queue-reconstruction-by-height/406-queue-reconstruction-by-height.cpp
+++ 406-queue-reconstruction-by-height/406-queue-reconstruction-by-height.cpp
@@ -1,18 +1,16 @@
 class Solution {
 public:
-   
     vector<vector<int>> reconstructQueue(vector<vector<int>>& people) {
         //queue reconstruction
-         sort(begin(people),end(people),comp);
-        vector<vector<int>> res;
-        for(auto &x:people){
-            res.insert(begin(res)+x[1],x);
+        sort(begin(people),end(people),[&](auto a,auto b){
+            if(a[0] != b[0])
+                return a[0]>b[0];
+            else return a[1]<b[1];
+        });
+        vector<vector<int>> ans;
+        for(auto x:people){
+            ans.insert(ans.begin()+x[1],x);
         }
-        return res;
-    }
-         static bool comp(vector<int> &p1,vector<int> &p2){
-        if(p1[0]!=p2[0]) return p1[0]>p2[0];
-        else return p1[1]<p2[1];
-    
+        return ans;
     }
 };
