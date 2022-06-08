65d845a43349bf10c1891166990e206045da6c8c
Pranav Rajveer <55043383+Neon-20@users.noreply.github.com>
2022-06-08 12:26:18 +0530

    Time: 13 ms (50.87%), Space: 11 MB (17.20%) - LeetHub

--- 674-longest-continuous-increasing-subsequence/674-longest-continuous-increasing-subsequence.cpp
+++ 674-longest-continuous-increasing-subsequence/674-longest-continuous-increasing-subsequence.cpp
@@ -6,7 +6,7 @@ public:
         int res=0;
         int cnt=0;
         for(int i=0;i<n;i++){
-            if(i==0 or nums[i-1]<nums[i]) res=max(res,++cnt);
+            if(i == 0 or nums[i-1]<nums[i]) res=max(res,++cnt);
             else cnt=1;
         }
         return res;
