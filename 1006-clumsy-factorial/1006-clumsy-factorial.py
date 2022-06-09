f876521cb8c2e6d21ef22faf546cf08fb76e5090
Pranav Rajveer <55043383+Neon-20@users.noreply.github.com>
2022-06-09 19:47:58 +0530

    Time: 57 ms (67.94%), Space: 13.8 MB (81.68%) - LeetHub

--- 1006-clumsy-factorial/1006-clumsy-factorial.py
+++ 1006-clumsy-factorial/1006-clumsy-factorial.py
@@ -2,17 +2,17 @@ class Solution:
     def clumsy(self, n: int) -> int:
         if(n == 1):
             return 1
-        elif (n == 2):
+        if (n == 2):
             return 2
-        elif(n == 3):
+        if(n == 3):
             return 6
-        elif(n == 4):
+        if(n == 4):
             return 7
-        elif(n%4==1):
+        if(n%4==1):
             return n+2
-        elif(n%4==2):
+        if(n%4==2):
             return n+2
-        elif(n%4==3):
+        if(n%4==3):
             return n-1
         return n+1
 #     tc->o(1)
