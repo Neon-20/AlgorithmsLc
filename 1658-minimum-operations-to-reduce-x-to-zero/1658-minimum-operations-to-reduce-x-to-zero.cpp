5b41b72a3e0c3d96ac695c936fbb92e8ff92ef5e
Pranav Rajveer <55043383+Neon-20@users.noreply.github.com>
2022-06-15 08:44:54 +0530

    Time: 201 ms (84.32%), Space: 98.4 MB (84.07%) - LeetHub

--- 1658-minimum-operations-to-reduce-x-to-zero/1658-minimum-operations-to-reduce-x-to-zero.cpp
+++ 1658-minimum-operations-to-reduce-x-to-zero/1658-minimum-operations-to-reduce-x-to-zero.cpp
@@ -2,21 +2,23 @@ class Solution {
 public:
     int minOperations(vector<int>& nums, int x) {
         int n=size(nums);
+        //we just find the middle of the array like
+        //total-x k barabar ya bada
+        int sum=0;
+        int right=0;
+        int len=-1;
         int total=accumulate(begin(nums),end(nums),0);
         if(total<x) return -1;
-        int sum=0;
-        int j=0;
-        int max_len=-1;
         for(int i=0;i<n;i++){
             sum+=nums[i];
             while(sum>total-x){
-                sum-=nums[j++];
+                sum-=nums[right++];
             }
             if(sum == (total - x)){
-                max_len=max(max_len,i-j+1);
+                len=max(len,i-right+1);
             }
         }
-        if(max_len==-1) return -1;
-        else return n-max_len;
+               if(len == -1) return -1;
+               else return n-len;
     } 
 };
