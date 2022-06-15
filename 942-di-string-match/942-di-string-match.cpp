5d0d4587f3c8a32391d617e698727c4b35a3d1bb
Pranav Rajveer <55043383+Neon-20@users.noreply.github.com>
2022-06-15 19:50:53 +0530

    Time: 11 ms (29.53%), Space: 9 MB (27.18%) - LeetHub

---
+++ 942-di-string-match/942-di-string-match.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<int> diStringMatch(string s) {
+        int n=s.size();
+        vector<int> ans;
+        int i=0;//two pointers1
+        int j=s.length();
+        for(int k=0;k<n;k++){
+            if(s[k] == 'I'){// 0 1 
+                ans.push_back(i);
+                i++;// 1 
+            }
+            else if(s[k] == 'D'){ //4 3
+                ans.push_back(j);
+                j--;
+            }
+        }
+        if(s[s.size()-1] == 'I'){
+            ans.push_back(i);
+        }
+        else ans.push_back(j);
+        return ans;
+    }
+};
