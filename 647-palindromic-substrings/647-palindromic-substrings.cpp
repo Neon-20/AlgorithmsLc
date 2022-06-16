e2f668e0920c7be5d85b92629b21f1c4beb6bd85
Pranav Rajveer <55043383+Neon-20@users.noreply.github.com>
2022-06-16 14:54:36 +0530

    Time: 8 ms (66.93%), Space: 6.2 MB (96.06%) - LeetHub

--- 647-palindromic-substrings/647-palindromic-substrings.cpp
+++ 647-palindromic-substrings/647-palindromic-substrings.cpp
@@ -1,23 +1,22 @@
 class Solution {
 public: 
-    int dp[1005][1005];
-    int found(string &s, int i,int j){
-        if(i>=j) return 1;
-        if(dp[i][j]!=-1) return dp[i][j];
-        if(s[i]!=s[j]) return 0;
-        int ans=0;
-        ans=found(s,i+1,j-1);
-         dp[i][j]=ans;
-        return ans;
+       int cnt=0;
+ void get(string &s,int left,int right){
+  while(left>=0 and right<s.length() and s[left] == s[right])
+  {
+       cnt++;
+        left--;
+        right++;
+  }
+     // return cnt;
     }
     int countSubstrings(string s) {
-       memset(dp,-1,sizeof(dp));
-        int cnt=0;
-       for(int i=0;i<size(s);i++){
-           for(int j=i;j<size(s);j++){
-               if(found(s,i,j)) cnt++;
-           }
-       }
+       //using two pointer normal
+        // int cnt=0;
+        for(int i=0;i<s.length();i++){
+            get(s,i,i);
+            get(s,i,i+1);
+        }
         return cnt;
     }
 };
