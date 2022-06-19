7d5f72cb74522db7aa77d6e93ee3a410c4e67205
Pranav Rajveer <55043383+Neon-20@users.noreply.github.com>
2022-06-19 19:35:34 +0530

    Time: 24 ms (80.37%), Space: 10.5 MB (47.37%) - LeetHub

--- 696-count-binary-substrings/696-count-binary-substrings.cpp
+++ 696-count-binary-substrings/696-count-binary-substrings.cpp
@@ -4,24 +4,30 @@ public:
         int n=s.length();
         int cnt=1;
         int prefix=0;
-        int sum=0;
-        for(int i=1;i<s.length();i++){
-            if(s[i] == s[i-1]){
+        int ans=0;
+        for(int i=1;i<s.length();i++){//i=2
+            if(s[i] == s[i-1]){//consecutive
                 cnt++;
             }
             else{
-                sum+=min(prefix,cnt);
+                ans+=min(prefix,cnt);
                 prefix=cnt;
                 cnt=1;
             }
         }
-        sum+=min(cnt,prefix);
-        return sum;
+        ans+=min(cnt,prefix);
+        return ans;
     }
 };
 
-   // 0 0 1 1 0 0 1 1
-    // s[i] == s[i-1]
+   // 0 0 1 1 0 0 1 1->
+// cnt=2;
+// ans=2+2=4+2=6
+// prefix=2
+
+   
+
+
 
 
  
