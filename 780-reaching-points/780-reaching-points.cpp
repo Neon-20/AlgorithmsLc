5133dbea2ef5ab54d2c5fec50e7ae11dfb855ab7
Pranav Rajveer <55043383+Neon-20@users.noreply.github.com>
2022-06-24 09:40:49 +0530

    Time: 3 ms (36.89%), Space: 6 MB (12.95%) - LeetHub

--- 780-reaching-points/780-reaching-points.cpp
+++ 780-reaching-points/780-reaching-points.cpp
@@ -5,11 +5,11 @@ public:
         while(tx>=sx and ty>=sy){
             if(tx>ty){
               if(ty == sy) return (tx-sx)%ty==0;
-              else tx%=ty;
+                else tx%=ty;
             }
             else{
                 if(tx == sx) return (ty-sy)%tx==0;
-              else ty%=tx;
+                else ty%=tx;
             }
         }
         return false;
