42ec84c9905b0ce061f349defcfd4fe455d9ad9f
Pranav Rajveer <55043383+Neon-20@users.noreply.github.com>
2022-06-24 09:04:24 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (12.95%) - LeetHub

---
+++ 780-reaching-points/780-reaching-points.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool reachingPoints(int sx, int sy, int tx, int ty) {
+        //can be solved using heap data structure or maths
+        while(tx>=sx and ty>=sy){
+            if(tx>ty){
+              if(ty == sy) return (tx-sx)%ty==0;
+              else tx%=ty;
+            }
+            else{
+                if(tx == sx) return (ty-sy)%tx==0;
+              else ty%=tx;
+            }
+        }
+        return false;
+    }
+};
