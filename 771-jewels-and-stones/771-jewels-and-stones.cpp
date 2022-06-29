0fad85a89d4dc36fe122bf4a5bb3bcf25d675930
Pranav Rajveer <55043383+Neon-20@users.noreply.github.com>
2022-06-29 13:38:10 +0530

    Time: 9 ms (8.78%), Space: 6.4 MB (45.72%) - LeetHub

--- 771-jewels-and-stones/771-jewels-and-stones.cpp
+++ 771-jewels-and-stones/771-jewels-and-stones.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int numJewelsInStones(string jewels, string stones) {
-     unordered_set<int> s(begin(jewels),end(jewels));
+     set<int> s(begin(jewels),end(jewels));
         int ans=0;
         for(char c:stones){
             if(s.count(c)) ans++;
