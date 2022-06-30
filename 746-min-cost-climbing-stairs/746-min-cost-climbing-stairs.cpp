7a96e2bf650ae222af43f1f045ce9aa0c5704095
Pranav Rajveer <55043383+Neon-20@users.noreply.github.com>
2022-06-30 12:02:53 +0530

    Time: 8 ms (67.08%), Space: 13.7 MB (51.18%) - LeetHub

--- 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.cpp
+++ 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.cpp
@@ -1,16 +1,18 @@
 class Solution {
 public:
-    int solve(int index,vector<int> &dp,vector<int> &cost){
-        if(index>=size(cost)) return 0;
-        if(dp[index]!=-1) return dp[index];
-        int op1 = cost[index] + solve(index+1,dp,cost);
-        int op2 = cost[index] + solve(index+2,dp,cost);
-        dp[index]=min(op1,op2);
-        return min(op1,op2);
+      
+    int dp[1005];
+    int helper(vector<int> &cost,int n){
+        if(n<2) return cost[n];
+        if(dp[n]!=-1) return dp[n];
+        int ans=0;
+        ans=cost[n]+min(helper(cost,n-1),helper(cost,n-2));
+        dp[n]=ans;
+        return ans;
     }
     int minCostClimbingStairs(vector<int>& cost) {
         int n=size(cost);
-        vector<int> dp(n+1,-1);
-        return  min(solve(0,dp,cost),solve(1,dp,cost));
+        memset(dp,-1,sizeof(dp));
+        return min(helper(cost,n-1),helper(cost,n-2));
     }
 };
