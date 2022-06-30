7564bab8de6b3c8c8ed6a140bc2d8695332e26a3
Pranav Rajveer <55043383+Neon-20@users.noreply.github.com>
2022-06-30 11:43:09 +0530

    Time: 1114 ms (23.56%), Space: 12.5 MB (66.44%) - LeetHub

---
+++ 873-length-of-longest-fibonacci-subsequence/873-length-of-longest-fibonacci-subsequence.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int lenLongestFibSubseq(vector<int>& nums) {
+        int n=nums.size();
+        // longest subsequence
+        // 2 ka sum a[i] hona chahiye
+        set<int> s(begin(nums),end(nums));
+        int cnt=0;
+        int len=INT_MIN;
+        for(int i=0;i<n;i++){
+            for(int j=i+1;j<n;j++){
+                int a=nums[i],b=nums[j];
+                int c=a+b;
+                cnt=2;
+                while(s.count(c)){
+                    cnt++;
+                    a=b;
+                    b=c;
+                    c=a+b;
+                }
+                len=max(len,cnt);
+            }
+        }
+         if(len>2) return len;
+        else return 0;
+    }
+};
