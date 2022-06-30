ae8ec7b2379291948ad59f869eb23b3c51e6b446
Pranav Rajveer <55043383+Neon-20@users.noreply.github.com>
2022-06-30 11:51:19 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (41.41%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.cpp
+++ 509-fibonacci-number/509-fibonacci-number.cpp
@@ -4,10 +4,11 @@ public:
       int f[n+1];
         if(n<2) return n;
         int a=0,b=1,c;
-        while(n-->1){
+        while(n>1){
             c=a+b;
             a=b;
             b=c;
+            n--;
         }
         return b;
     }
