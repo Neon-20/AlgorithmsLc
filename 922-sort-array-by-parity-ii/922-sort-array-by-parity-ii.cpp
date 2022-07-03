67ae5ea3af46e2092d842b45bee6d6eab3aa842c
Pranav Rajveer <55043383+Neon-20@users.noreply.github.com>
2022-07-03 13:17:22 +0530

    Time: 46 ms (17.97%), Space: 21.4 MB (88.40%) - LeetHub

---
+++ 922-sort-array-by-parity-ii/922-sort-array-by-parity-ii.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<int> sortArrayByParityII(vector<int>& nums) {
+        //sort array by parity
+        // now we have as:
+        int n=size(nums);
+        vector<int> ans(n,0);
+        int odd=1;
+        int even=0;
+        for(int x:nums){
+            if(x%2 == 0){
+                ans[even]=x;
+                even+=2;
+            }
+            if(x%2){
+                ans[odd]=x;
+                odd+=2;
+            }
+        }
+        return ans;
+    }
+};
