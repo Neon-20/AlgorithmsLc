76accf4aba8f939a22090d83c80b1e0ec0632256
Pranav Rajveer <55043383+Neon-20@users.noreply.github.com>
2022-07-06 17:39:44 +0530

    Time: 56 ms (17.23%), Space: 10.9 MB (61.28%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -1,22 +1,22 @@
 class Solution {
 public:
-    // ......_......
- int helper(int i,int j,string &s,string &t,vector<vector<int>>&dp){
+    int dp[1005][1005];
+ int helper(int i,int j,string &s,string &t){
      if(i >= size(s) or j>=size(t)) return 0;//base case
      if(dp[i][j]!=-1) return dp[i][j];
      int ans=0;
      if(s[i] == t[j]){
-        ans=1+helper(i+1,j+1,s,t,dp);
+        ans=1+helper(i+1,j+1,s,t);
      }   
         else{
-ans=max(helper(i,j+1,s,t,dp),helper(i+1,j,s,t,dp));
+ans=max(helper(i,j+1,s,t),helper(i+1,j,s,t));
         }
-     dp[i][j]=ans;
+     dp[i][j]=ans;//memoization
      return ans;
      
  }
     int longestCommonSubsequence(string s, string t) {
-    vector<vector<int>> dp(size(s)+1,vector<int>(size(t)+1,-1));
-        return helper(0,0,s,t,dp);
+    memset(dp,-1,sizeof(dp));
+        return helper(0,0,s,t);
     }   
 };
