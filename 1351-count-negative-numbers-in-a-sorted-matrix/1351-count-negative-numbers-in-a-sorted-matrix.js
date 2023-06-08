821bbe97cd8a116c2fa62dc3617ec88fb213e818
Pranav Rajveer <55043383+Neon-20@users.noreply.github.com>
2023-06-08 08:59:04 +0530

    Time: 65 ms (52.08%), Space: 44.8 MB (20.22%) - LeetHub

--- 1351-count-negative-numbers-in-a-sorted-matrix/1351-count-negative-numbers-in-a-sorted-matrix.js
+++ 1351-count-negative-numbers-in-a-sorted-matrix/1351-count-negative-numbers-in-a-sorted-matrix.js
@@ -3,12 +3,11 @@
  * @return {number}
  */
 var countNegatives = function(grid) {
-    //2d grid
-    let cnt=0;
-    for(let i=0;i<grid.length;i++){
-        for(let j=0;j<grid[0].length;j++){
-            if(grid[i][j]<0) cnt++;
+   let cnt=0;
+    for(let value of grid){
+        for(let in_value of value){
+            if(in_value<0) cnt++;
         }
     }
-  return cnt;
+    return cnt;
 };
