62c6894abddc737eb73008b83c113f56a5db5849
Pranav Rajveer <55043383+Neon-20@users.noreply.github.com>
2023-06-08 15:27:19 +0530

    Time: 71 ms (12.52%), Space: 42.5 MB (8.01%) - LeetHub

--- 2627-debounce/2627-debounce.js
+++ 2627-debounce/2627-debounce.js
@@ -5,7 +5,7 @@
  */
 function debounce(fn,t) {
     let timer;
-    return function(...args) {
+    return function(...args){
         clearTimeout(timer);
         timer=setTimeout(()=>{
             fn(...args)
