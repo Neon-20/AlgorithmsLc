be41ab8daaa60e7411d10365a386f435f8b79d4c
Pranav Rajveer <55043383+Neon-20@users.noreply.github.com>
2023-06-14 00:56:12 +0530

    Time: 73 ms (6.34%), Space: 42.5 MB (53.96%) - LeetHub

---
+++ 2626-array-reduce-transformation/2626-array-reduce-transformation.js
@@ -0,0 +1,12 @@
+/**
+ * @param {number[]} nums
+ * @param {Function} fn
+ * @param {number} init
+ * @return {number}
+ */
+
+//reduce to following expression
+
+const reduce = (nums,fn,init) =>{
+   return nums.reduce(fn,init)
+}
