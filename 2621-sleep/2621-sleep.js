61dc1af4d4eb0bb0c37255762dd57e949c9a57be
Pranav Rajveer <55043383+Neon-20@users.noreply.github.com>
2023-06-13 19:50:34 +0530

    Time: 49 ms (95.57%), Space: 42.1 MB (12.37%) - LeetHub

---
+++ 2621-sleep/2621-sleep.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number} millis
+ */
+ //Write a function which sleeps for certain amount of time.
+async function sleep(millis) {
+  return new Promise(resolve =>{
+     setTimeout(resolve,millis)
+  })
+}
+
+
+/** 
+ * let t = Date.now()
+ * sleep(100).then(() => console.log(Date.now() - t)) // 100
+ */
