82e3c1748b3f2e46b8555df656bba4a57cd8745a
Pranav Rajveer <55043383+Neon-20@users.noreply.github.com>
2023-06-13 19:41:43 +0530

    Time: 70 ms (44.12%), Space: 44.7 MB (60.02%) - LeetHub

---
+++ 2665-counter-ii/2665-counter-ii.js
@@ -0,0 +1,29 @@
+/**
+ * @param {integer} init
+ * @return { increment: Function, decrement: Function, reset: Function }
+ */
+
+const createCounter = (init) =>{
+ let count = init
+function increment(){
+    return ++count
+ }
+function decrement(){
+    return --count
+ }
+function reset(){
+    count=init
+    return count
+ }
+    return {
+        increment,decrement,reset
+    }
+}
+
+
+/**
+ * const counter = createCounter(5)
+ * counter.increment(); // 6
+ * counter.reset(); // 5
+ * counter.decrement(); // 4
+ */
