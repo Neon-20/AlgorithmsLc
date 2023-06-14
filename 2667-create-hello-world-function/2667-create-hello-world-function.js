91e0cf2f33c1ba25001159786efc76dfb446765d
Pranav Rajveer <55043383+Neon-20@users.noreply.github.com>
2023-06-15 02:05:39 +0530

    Time: 50 ms (88.04%), Space: 41.8 MB (39.82%) - LeetHub

---
+++ 2667-create-hello-world-function/2667-create-hello-world-function.js
@@ -0,0 +1,14 @@
+/**
+ * @return {Function}
+ */
+
+  const createHelloWorld = function() {
+    return function(...args){
+        return "Hello World";
+    }
+  }
+
+/**
+ * const f = createHelloWorld();
+ * f(); // "Hello World"
+ */
