d10b21c425963156733d42064aaba0b865b517c6
Pranav Rajveer <55043383+Neon-20@users.noreply.github.com>
2023-06-23 13:54:06 +0530

    Time: 46 ms (96.99%), Space: 42.1 MB (47.94%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.js
+++ 0058-length-of-last-word/0058-length-of-last-word.js
@@ -6,6 +6,6 @@ var lengthOfLastWord = function(s) {
     //length of last word
     let n=s.length
     const spl = s.split(" ")
-    const result = spl.filter(x=>x.length !== 0)
+        const result = spl.filter(x=>x.length !== 0)
     return result.pop().length
 };
