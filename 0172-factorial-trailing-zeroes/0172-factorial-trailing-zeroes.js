18f6d4eb3870354fceec1e62bb5aaea93f5edefb
Pranav Rajveer <55043383+Neon-20@users.noreply.github.com>
2023-06-24 16:39:38 +0530

    Time: 54 ms (94.04%), Space: 41.7 MB (96.87%) - LeetHub

---
+++ 0172-factorial-trailing-zeroes/0172-factorial-trailing-zeroes.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var trailingZeroes = function(n) {
+    let count = 0;
+    for(let i=5;i<=n;i*=5){
+        count+=Math.floor(n/i);
+    }
+    return count;
+    
+};
+//Multiple of 5 trick
