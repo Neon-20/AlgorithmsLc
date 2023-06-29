ddd8529d67497389a0418ad3ac5ffcf046de01b5
Pranav Rajveer <rajveer23pr@gmail.com>
2023-06-29 11:11:17 +0530

    Time: 71 ms (14.75%), Space: 42.3 MB (14.22%) - LeetHub

--- 2627-debounce/2627-debounce.js
+++ 2627-debounce/2627-debounce.js
@@ -7,7 +7,7 @@ function debounce(fn,t=1000) {
     let timer;
     return function(...args){
         clearTimeout(timer);
-        timer = setTimeout(()=>fn(...args),t);
+        timer = setTimeout(()=>{fn(...args)},t);
     }
 }
 
