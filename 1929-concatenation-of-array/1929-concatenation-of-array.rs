f546df1a1e492ebe115e0f278413900b14677b9c
Pranav Rajveer <rajveer23pr@gmail.com>
2023-09-27 21:23:02 +0530

    Time: 4 ms (80.71%), Space: 2 MB (100.00%) - LeetHub

--- 1929-concatenation-of-array/1929-concatenation-of-array.rs
+++ 1929-concatenation-of-array/1929-concatenation-of-array.rs
@@ -1,10 +1,6 @@
 impl Solution {
-    pub fn get_concatenation(mut nums: Vec<i32>) -> Vec<i32> {
-      let n = nums.len();
-      for i in 0..n{
-          nums.push(nums[i]);
-        }
-       return nums;
+    pub fn get_concatenation(nums: Vec<i32>) -> Vec<i32> {
+      nums.repeat(2)
     }
 }
 // data types in rust
