235b6cb9ab59db1787012cdb8db312c3a47369c9
Pranav Rajveer <rajveer23pr@gmail.com>
2023-09-27 21:20:43 +0530

    Time: 4 ms (80.71%), Space: 2.2 MB (45.69%) - LeetHub

---
+++ 1929-concatenation-of-array/1929-concatenation-of-array.rs
@@ -0,0 +1,16 @@
+impl Solution {
+    pub fn get_concatenation(mut nums: Vec<i32>) -> Vec<i32> {
+      let n = nums.len();
+      for i in 0..n{
+          nums.push(nums[i]);
+        }
+         nums
+    }
+}
+// data types in rust
+// we dont hav string data type in rust
+
+// i32 = integer of 32 bits
+// i64 = integer of 64 bits
+// bool = boolean
+// char = character
