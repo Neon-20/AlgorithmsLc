9bfd2f0f5ff1d78d753400be963b7dbe7d09c7d7
Pranav Rajveer <rajveer23pr@gmail.com>
2023-10-01 00:40:45 +0530

    Time: 6 ms (60.00%), Space: 4.8 MB (80.00%) - LeetHub

--- 0456-132-pattern/0456-132-pattern.rs
+++ 0456-132-pattern/0456-132-pattern.rs
@@ -1,18 +1,18 @@
 impl Solution {
     pub fn find132pattern(nums: Vec<i32>) -> bool {
-    // let n = nums.len();
-    let mut s3 = i32::MIN;
-    let mut stack: Vec<i32> = Vec::new();
-    for &num in nums.iter().rev(){
-        if num<s3 {return true;}
-        else
-        {while !stack.is_empty() && num>*stack.last().unwrap(){
-            s3 = stack.pop().unwrap();
+     let mut store = i32::MIN;
+     let mut st: Vec<i32> = Vec::new();
+     for &num in nums.iter().rev(){
+         if num < store {
+             return true;
+         }
+         else{
+             while !st.is_empty() && num > *st.last().unwrap(){
+                 store = st.pop().unwrap();
+             }
+             st.push(num)
+         }
         }
-        stack.push(num);
-        }
-    }
         false
-        
     }
 }
