62fd3d1a394b977795442bddafa534a45013f6cf
Pranav Rajveer <rajveer23pr@gmail.com>
2023-10-05 02:30:53 +0530

    Time: 193 ms (20.81%), Space: 131.9 MB (5.79%) - LeetHub

---
+++ 0706-design-hashmap/0706-design-hashmap.js
@@ -0,0 +1,15 @@
+class MyHashMap{
+    constructor(){
+        this.data = new Array(1000001);
+    }
+    put(key,val){
+        this.data[key]=val;
+    }
+    get(key){
+        let val = this.data[key];
+        return val!==undefined ? val:-1;
+    }
+    remove(key){
+        delete this.data[key];
+    }
+}
