c7f29301b3e695b2f7b4f27743331ac3356df1cb
Pranav Rajveer <rajveer23pr@gmail.com>
2023-10-06 16:59:54 +0530

    Time: 2 ms (49.96%), Space: 6.4 MB (36.38%) - LeetHub

---
+++ 0343-integer-break/0343-integer-break.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    long long integerBreak(long long n) {
+    // either of the numbers should be larger
+     if(n==2) return 1;
+     if(n==3) return 2;
+     if(n==4) return 4;
+     if(n==5) return 6;
+     if(n==6) return 9;
+     return 3*integerBreak(n-3);
+    }
+};
+// 10 -> 10 - 3 * 3
