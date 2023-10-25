52799cb5192913609abece9965aa502a5d26af00
Pranav Rajveer <rajveer23pr@gmail.com>
2023-10-26 03:30:33 +0530

    Time: 11 ms (81.35%), Space: 24.6 MB (20.71%) - LeetHub

--- 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.cpp
+++ 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.cpp
@@ -9,21 +9,20 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+//preorder = root -> left -> right
 class Solution {
 public:
+    void preOrder(TreeNode* node,vector<int>& ans){
+        if(!node) return;
+        ans.push_back(node->val);
+        preOrder(node->left,ans);
+        preOrder(node->right,ans);
+    }
     int kthSmallest(TreeNode* root, int k) {
-        if(!root) return {};
-        queue<TreeNode*> q;
-        q.push(root);
-        priority_queue<int> pq;
-        while(!q.empty()){
-            TreeNode* node = q.front(); q.pop();
-            pq.push(node->val);
-            if(pq.size()>k) pq.pop();
-            if(node->left) q.push(node->left);
-            if(node->right) q.push(node->right);
-        }
-        return pq.top();
+       vector<int> ans;
+       preOrder(root,ans);
+       sort(begin(ans),end(ans));
+       return ans[k-1];
     }
 };
 
