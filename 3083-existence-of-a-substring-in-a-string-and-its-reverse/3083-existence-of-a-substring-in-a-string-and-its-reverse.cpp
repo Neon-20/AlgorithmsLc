4db6717370edc5ae53bcfbd098eb392789caa4d6
Pranav Rajveer <rajveer23pr@gmail.com>
2024-03-19 11:59:54 +0530

    Time: 0 ms (100.00%), Space: 7.8 MB (93.49%) - LeetHub

--- 3083-existence-of-a-substring-in-a-string-and-its-reverse/3083-existence-of-a-substring-in-a-string-and-its-reverse.cpp
+++ 3083-existence-of-a-substring-in-a-string-and-its-reverse/3083-existence-of-a-substring-in-a-string-and-its-reverse.cpp
@@ -2,9 +2,9 @@ class Solution {
 public:
    
     bool isSubstringPresent(string s) {
-        for(int i=0;i+1<s.size();i++){
-            for(int j=0;j+1<s.size();j++){
-                if(s[i] == s[j+1] && s[i+1] == s[j]){
+        for(int i=0;i<s.size();i++){
+            for(int j=0;j<s.size();j++){
+                if(s[i]==s[j+1] and s[i+1] == s[j]){
                     return true;
                 }
             }
