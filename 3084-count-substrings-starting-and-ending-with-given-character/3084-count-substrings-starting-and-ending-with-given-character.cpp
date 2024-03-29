bdb3d48cb996a894f7c383bd48e5e273071a068c
Pranav Rajveer <rajveer23pr@gmail.com>
2024-03-29 14:39:37 +0530

    Time: 8 ms (91.94%), Space: 11.1 MB (72.67%) - LeetHub

--- 3084-count-substrings-starting-and-ending-with-given-character/3084-count-substrings-starting-and-ending-with-given-character.cpp
+++ 3084-count-substrings-starting-and-ending-with-given-character/3084-count-substrings-starting-and-ending-with-given-character.cpp
@@ -1,15 +1,14 @@
 class Solution {
 public:
     long long countSubstrings(string s, char c) {
-        long long n = s.size();
-        long long ans = 0;
-        long long cnt = 0;
-        for(int i=0;i<n;i++){
-            if(s[i]==c){
-                cnt++;
-                ans+=cnt;
-            }
-        }
-        return ans;
+       long long ans = 0;
+       long long cnt = 0;
+       for(auto x:s){
+           if(x == c){
+               cnt++;
+           }
+       }
+        return 1ll*(cnt)*(cnt+1)/2;
     }
 };
+// start and end with c
