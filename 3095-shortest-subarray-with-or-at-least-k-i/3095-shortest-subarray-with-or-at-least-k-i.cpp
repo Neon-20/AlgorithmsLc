495e3cb322dd99d350ec93cbececf2cb4919e1a1
Pranav Rajveer <rajveer23pr@gmail.com>
2024-03-30 23:42:14 +0530

    Time: 8 ms (33.33%), Space: 26 MB (16.67%) - LeetHub

---
+++ 3095-shortest-subarray-with-or-at-least-k-i/3095-shortest-subarray-with-or-at-least-k-i.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int minimumSubarrayLength(vector<int>& nums, int k) {
+        int n = nums.size();
+        // LENGTH OF SPECIAL SUBARRAY
+        int ans = 1e9;
+        for(int i=0;i<n;i++){
+            int x = 0;
+            for(int j=i;j<n;j++){
+                x|=nums[j];
+                if(x>=k){
+                    ans = min(ans,j-i+1);
+                }
+            }
+        }
+        return (ans == 1e9) ? -1 : ans;
+    }
+};
