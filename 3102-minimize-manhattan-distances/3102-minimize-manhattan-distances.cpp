77600cb6cb0e5ce875ec213e919785290c0072e7
Pranav Rajveer <rajveer23pr@gmail.com>
2024-04-01 21:47:56 +0530

    Time: 1024 ms (16.67%), Space: 239.6 MB (16.67%) - LeetHub

--- 3102-minimize-manhattan-distances/3102-minimize-manhattan-distances.cpp
+++ 3102-minimize-manhattan-distances/3102-minimize-manhattan-distances.cpp
@@ -1,34 +1,31 @@
 class Solution {
 public:
-    int minimumDistance(vector<vector<int>>& p) {
-       //challenging question it was
-       //all about manhattan distances over here
-//         max( | x[i]-x[j] | + | y[i]-y[j] | )
-      int n = p.size();
-      set<pair<int,int>> s,e;
-      for(int i=0;i<n;i++){
-          int x = p[i][0]+p[i][1];
-          int y = p[i][0]-p[i][1];
-          s.insert({x,i});
-          e.insert({y,i});
-      }
-        int ans = INT_MAX; 
-        for(int i=0;i<n;i++){
-            int x = p[i][0]+p[i][1];
-            int y = p[i][0]-p[i][1];
-            s.erase({x,i});
-            e.erase({y,i});
+    int minimumDistance(vector<vector<int>>& points) {
+        int n = points.size();
+        multiset<int> s, e;
+        for (auto it : points) {
+            int x = it[0] + it[1];
+            int y = it[0] - it[1];
+            s.insert(x);
+            e.insert(y);
+        }
+        int ans = INT_MAX;
+        for (int i = 0; i < n; i++) {
+            int x = points[i][0] + points[i][1];
+            int y = points[i][0] - points[i][1];
+            s.erase(s.find(x));
+            e.erase(e.find(y));
             
-            auto [a,ie] = *s.begin();
-            auto [b,ig] = *s.rbegin();
-            auto [c,il] = *e.begin();
-            auto [k,im] = *e.rbegin();
+            auto a = *s.begin();
+            auto b = *s.rbegin();
+            auto c = *e.begin();
+            auto k = *e.rbegin();
             
-           int distance = max(b-a,k-c);
-            ans = min(distance,ans);
+            int distance = max(b - a, k - c);
+            ans = min(distance, ans);
             
-            s.insert({x,i});
-            e.insert({y,i});
+            s.insert(x);
+            e.insert(y);
         }
         return ans;
     }
