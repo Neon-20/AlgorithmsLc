cf57c6af1fb5d9a212d0c3a45dbaa2584ddae4bc
Pranav Rajveer <rajveer23pr@gmail.com>
2024-04-03 16:09:08 +0530

    Time: 147 ms (88.07%), Space: 21.1 MB (92.66%) - LeetHub

--- 2741-special-permutations/2741-special-permutations.cpp
+++ 2741-special-permutations/2741-special-permutations.cpp
@@ -1,24 +1,23 @@
 class Solution {
 public:
+    const int mod = 1e9+7;
     int dp[14][1<<14];
-    const int MOD = 1e9+7;
     int helper(int i,int mask,vector<int> &nums){
         int n = nums.size();
         if(mask == (1<<n) - 1){
             return 1;
         }
-        if(dp[i][mask]==-1){
+        if(dp[i][mask] == -1){
             dp[i][mask] = 0;
-        for(int j=0;j<n;j++){
-if((mask & (1<<j)) == 0 and (mask == 0 || nums[i]%nums[j]==0 || nums[j]%nums[i] == 0)){
-    dp[i][mask] = (dp[i][mask] + helper(j,mask+(1<<j),nums))%MOD;
-        }
+            for(int j=0;j<n;j++){
+if((mask & (1<<j)) == 0 && (mask == 0 or nums[j]%nums[i] == 0 or nums[i]%nums[j] == 0)){
+         dp[i][mask] = (dp[i][mask] + helper(j,mask+(1<<j),nums))%mod;
+                }
             }
         }
-        return dp[i][mask];
+        return dp[i][mask] ;
     }
     int specialPerm(vector<int>& nums) {
-        int n = nums.size();
         memset(dp,-1,sizeof(dp));
         return helper(0,0,nums);
     }
