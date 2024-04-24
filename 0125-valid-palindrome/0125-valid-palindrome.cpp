3e16d910bd1577db45d630cc8357171318218576
Pranav Rajveer <rajveer23pr@gmail.com>
2024-04-24 18:14:56 +0530

    Time: 11 ms (6.17%), Space: 8.9 MB (39.09%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -1,21 +1,21 @@
 class Solution {
 public:
     bool isPalindrome(string s) {
-      //remove all the shit and then solve it
-        int n = s.length();
-        int l = 0;
-        int r = n-1;
-        while(l<r){
-            while(l<r and !isalnum(s[l])){
-                l++;
-            }
-            while(l<r and !isalnum(s[r])){
-                r--;
-            }
-            if(tolower(s[l++])!=tolower(s[r--])){
-                return false;
-            }
+       //string pall or not
+    int n = s.length();
+        //remove spaces and special characters
+ s.erase(remove_if(s.begin(), s.end(), ::isspace), s.end());
+  s.erase(remove_if(s.begin(), s.end(), [](char c) {
+        return !isalnum(c);
+    }), s.end());
+        transform(s.begin(), s.end(), s.begin(), ::tolower);
+
+        string t = s;
+        reverse(begin(t),end(t));
+        cout<<t<<" ";
+        if(s==t){
+            return true;
         }
-        return true;
-    }
+        else return false;
+}
 };
