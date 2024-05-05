22c5375dcf48b41dd74cbef70954d714a2d7dd66
Pranav Rajveer <rajveer23pr@gmail.com>
2024-05-05 17:09:52 +0530

    Time: 12 ms (37.37%), Space: 16.4 MB (19.13%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -1,29 +1,24 @@
 class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
-        int n = matrix.size();// row
-        int m = matrix[0].size();//column
+        int n = matrix.size();
+        int m = matrix[0].size();
         vector<int> row(n,0);
         vector<int> col(m,0);
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
                 if(matrix[i][j]==0){
-                    row[i]=1; //marked
-                    col[j]=1; //marked
+                    row[i] = 1;
+                    col[j] = 1;
                 }
             }
         }
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-                if(row[i] or col[j]){
-                   matrix[i][j]=0;
+                if(row[i] || col[j]){
+                    matrix[i][j] = 0;
                 }
             }
         }
-        
     }
 };
-
-
-// strategy is to mark and attack 
-// mark with row and col arrays and attack after that
