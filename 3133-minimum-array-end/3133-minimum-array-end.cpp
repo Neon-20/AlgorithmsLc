70a926ecb441b5edded33b40e6722c96e60cdf0d
Pranav Rajveer <rajveer23pr@gmail.com>
2024-05-05 16:49:31 +0530

    Time: 1738 ms (6.94%), Space: 7.7 MB (82.00%) - LeetHub

--- 3133-minimum-array-end/3133-minimum-array-end.cpp
+++ 3133-minimum-array-end/3133-minimum-array-end.cpp
@@ -1,12 +1,12 @@
 class Solution {
 public:
     long long minEnd(int n, int x) {
-       //bit manipulation
-        long long res = x;
+        long long a = x;
         while(--n>0){
-            res = (res+1) | x;
+            a = (a+1) | x;
         }
-        return res;
+        return a;
     }
 };
+// all bitwise AND is x
 
