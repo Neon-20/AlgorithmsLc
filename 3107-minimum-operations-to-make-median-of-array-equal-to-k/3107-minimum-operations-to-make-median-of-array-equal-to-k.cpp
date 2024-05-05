f3fd181b4cc6f8c369a1050a20f6e13a4e572521
Pranav Rajveer <rajveer23pr@gmail.com>
2024-05-05 17:01:14 +0530

    Time: 142 ms (59.87%), Space: 89.1 MB (75.60%) - LeetHub

--- 3107-minimum-operations-to-make-median-of-array-equal-to-k/3107-minimum-operations-to-make-median-of-array-equal-to-k.cpp
+++ 3107-minimum-operations-to-make-median-of-array-equal-to-k/3107-minimum-operations-to-make-median-of-array-equal-to-k.cpp
@@ -1,18 +1,16 @@
 class Solution {
 public:
     long long minOperationsToMakeMedianK(vector<int>& nums, int k) {
-        //we dont need to sort over here right
-    int n = nums.size();
-    sort(begin(nums),end(nums));
+        sort(begin(nums),end(nums));
+        int n = nums.size();
+        //increase or decrease 
         long long ans = 0;
-    for(int i=0;i<=(int)nums.size()/2;i++){
-        if(nums[i]>k) ans+=nums[i]-k; // 2
-    }
-        for(int i=(int)nums.size()/2;i<n;i++){
+        for(int i=0;i<=n/2;i++){
+            if(nums[i]>k) ans+=nums[i]-k;
+        }
+        for(int i=n/2;i<n;i++){
             if(nums[i]<k) ans+=k-nums[i];
         }
         return ans;
     }
 };
-// 2,5,6,8,5
-// 2 5 5 6 8
