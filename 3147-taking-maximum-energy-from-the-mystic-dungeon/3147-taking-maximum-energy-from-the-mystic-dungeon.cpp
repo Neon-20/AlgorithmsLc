a9674ca223817847f72c3c6a5e53a267a92bf496
Pranav Rajveer <rajveer23pr@gmail.com>
2024-05-13 12:25:36 +0530

    Time: 144 ms (50.00%), Space: 149.7 MB (50.00%) - LeetHub

--- 3147-taking-maximum-energy-from-the-mystic-dungeon/3147-taking-maximum-energy-from-the-mystic-dungeon.cpp
+++ 3147-taking-maximum-energy-from-the-mystic-dungeon/3147-taking-maximum-energy-from-the-mystic-dungeon.cpp
@@ -1,17 +1,17 @@
 class Solution {
 public:
     int maximumEnergy(vector<int>& nums, int k) {
+        // max energy can gain
         int n = nums.size();
         vector<int> dp(n,INT_MIN);
         int ans = INT_MIN;
         for(int i=n-1;i>=0;i--){
-             dp[i] = nums[i];
+            dp[i]=nums[i];
             if(i+k<n){
-              dp[i]+=dp[i+k]; 
+                dp[i]+=dp[i+k];
             }
-            ans=max(ans,dp[i]);
+            ans = max(ans,dp[i]);
         }
         return ans;
     }
 };
-// 
