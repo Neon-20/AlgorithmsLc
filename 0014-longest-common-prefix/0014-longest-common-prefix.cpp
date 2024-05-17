81ba4fa9517ad5681079aad005d692980ef7b389
Pranav Rajveer <rajveer23pr@gmail.com>
2024-05-17 15:38:22 +0530

    Time: 5 ms (34.39%), Space: 11 MB (46.53%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.cpp
+++ 0014-longest-common-prefix/0014-longest-common-prefix.cpp
@@ -6,9 +6,7 @@ public:
         for(int i=1;i<n;i++){
             while(strs[i].find(prefix)!=0){
                 prefix = prefix.substr(0,prefix.length()-1);
-                if(prefix.length()==0){
-                    return "";
-                }
+                if(prefix.length()==0) return "";
             }
         }
         return prefix;
