36b5063c6215cc58e6abc642d1dc8a4c8775a43f
Pranav Rajveer <rajveer23pr@gmail.com>
2024-05-17 22:35:41 +0530

    Time: 0 ms (100.00%), Space: 11.9 MB (84.89%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.cpp
+++ 0035-search-insert-position/0035-search-insert-position.cpp
@@ -3,22 +3,6 @@ public:
     int searchInsert(vector<int>& nums, int target) {
         int n = nums.size();
         //binary search
-        int left = 0;
-        int right = n-1;
-        int ans = 0;
-        while(left<=right){
-            int mid = (right-left)+left/2;
-            if(nums[mid]==target){
-                return mid;
-            }
-            else if(nums[mid]>target){
-                right = mid-1;
-                // ans = mid;
-            }
-            else{
-                left = mid+1;
-            }
-        }
-        return left;
+        return std::lower_bound(begin(nums),end(nums),target)-begin(nums);
     }
 };
